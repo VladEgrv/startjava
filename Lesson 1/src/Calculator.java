f5850b020e127bdc0c48849e76c4e5805e3f9b87
vaegorov <vladegrv@yandex.ru>
2024-04-27 17:38:53 +0300

    HW Calculator corrected

--- Lesson 1/src/Calculator.java
+++ Lesson 1/src/Calculator.java
@@ -1,8 +1,8 @@
 public class Calculator {
     public static void main(String[] args) {
-        int a = 2;
-        int b = 1;
-        char sign = '%';
+        int a = 3;
+        int b = 3;
+        char sign = '^';
         int result = 0;
         if (sign == '+') {
             result = a + b;
@@ -13,8 +13,9 @@ public class Calculator {
         } else if (sign == '/') {
             result = a / b;
         } else if (sign == '^') {
+            result = 1;
             for (int i = 0; i < b; i++) {
-                result += a;
+                result *= a;
             }
         } else if (sign == '%') {
             result = a % b;
