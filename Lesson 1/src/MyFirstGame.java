bbefb038ad7b327f68491bad5bc9ac59513384d3
vaegorov <vladegrv@yandex.ru>
2024-04-27 18:00:40 +0300

    HW MyFirstGame added

---
+++ Lesson 1/src/MyFirstGame.java
@@ -0,0 +1,16 @@
+public class MyFirstGame {
+    public static void main(String[] args) {
+        int secretNumber = 1;
+        int userNumber = 48;
+        do {
+            if (userNumber > secretNumber) {
+                System.out.println("Число " + userNumber + " больше того, что загадал компьютер");
+                userNumber--;
+            } else if (userNumber < secretNumber) {
+                System.out.println("Число " + userNumber + " меньше того, что загадал компьютер");
+                userNumber++;
+            }
+        } while (userNumber != secretNumber);
+        System.out.println("Вы победили!");
+    }
+}
