5eadb7b33a4b7eb51a99be840d62cef819eef48a
vaegorov <vladegrv@yandex.ru>
2024-05-02 12:14:45 +0300

    HW Person added

---
+++ Lesson 2/src/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    char gender = 'M';
+    String name = "Иван";
+    float height = 183f;
+    float weight = 84.4f;
+    int age = 30;
+    
+    public void walk() {
+        System.out.println(name + " идет");
+    }
+    
+    public void run() {
+        System.out.println(name + " бежит");
+    }
+    
+    public void sit() {
+        System.out.println(name + " сидит");
+    }
+    
+    public void talk() {
+        System.out.println(name + " говорит");
+    }
+    
+    public void teach() {
+        System.out.println(name + " учит");
+    }
+}
